71bb99d364301fe3adf2855829cfba980ffa7f66
Tomas Schuster <ts2525@hotmail.com>
2021-12-28 01:28:04 -0300

    version1

---
+++ routes/index.js
@@ -0,0 +1,23 @@
+const { Router } = require('express');
+const router = Router();
+ 
+//Raiz
+router.get('/', (req, res) => {    
+    res.send(
+       `<p>Bienvenido a la API de Los Simpsons Argentina.</p>
+       <p>Para consultar los servicios disponibles ingresa a: <a href="localhost:4000" >Swagger</a></p>
+       
+       `
+    );
+})
+
+router.get('/api/allLinks', (req, res) => { 
+    const fs = require('fs');
+
+    let rawdata = fs.readFileSync('./routes/caps.json');
+    let caps = JSON.parse(rawdata);
+
+    res.json(caps);
+})
+
+module.exports = router;
