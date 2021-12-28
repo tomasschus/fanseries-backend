d930b7af7bc847d20a7664b3da3cd3ebd1a08f6c
Tomas Schuster <55855002+tomasschus@users.noreply.github.com>
2021-12-28 01:19:38 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const morgan=require('morgan');
+ 
+//Configuraciones
+app.set('port', process.env.PORT || 4000);
+app.set('json spaces', 2)
+ 
+//Middleware
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+ 
+//routes
+app.use(require('./routes/index'));
+
+ 
+//Iniciando el servidor
+app.listen(app.get('port'),()=>{
+    console.log(`Server listening on port ${app.get('port')}`);
+});
